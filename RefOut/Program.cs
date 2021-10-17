f4b38fcbe71bd434c5bcd9f0084b4c3e9f2043d0
Akın KILIÇ <akin602000@gmail.com>
2021-10-17 14:34:26 +0300

    Add project files.

---
+++ RefOut/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace RefOut
+{
+    class Program
+    {
+        static void Main()
+        {
+            //ilk  baştta bir değişken ve iki tane metot oluşturalım.
+            int sayimiz = 3;
+            //oluşturduğumuz değişkeni ekrana yazdıralım.
+            Console.WriteLine("Başlangıç değeri: {0}", sayimiz);
+            //oluşturduğumuz motodu çağırarak değişkenimizin değerini ekrana yazdıralım.
+            Topla(sayimiz);
+            Console.WriteLine("Topla() sonrası: {0}", sayimiz);
+            //şimdide ref keyword lü metotdu çağırarak sonucu ekrana yazdıralım.
+            Topla2(ref sayimiz);
+            Console.WriteLine("Topla2() sonrası: {0}", sayimiz);
+
+            //normal metodu çağırdığımızda int olarak tanımladığımız değer tipli sayı metot içindeki parametreye atanır
+            //ve parametre üzerinde işlemler gerçekleşir bu yüzden asıl sayımızda değişiklik olmaz.
+            //daha sonrasında ise ref keywordu ile kullandığımız metodu çağırdığımızda ise ref keywordu sayesinde
+            //değer tipli olan int sayımız referans tip olarak davranarak oluşturduğumuz sayının ilk değeri değişir.
+            //bunun nedeni ise değer tipler stack içerisinde ve referans tipler heap içerisinde saklanır.
+            //Heap bölgesinde oluşturulan referans türlerine belleğin başlangıç adresi ile erişim sağlanır
+            //ve herhangi bir yerde yapılan değişiklik verinin değişmesine neden olur.
+            //ref anahtar kelimesini kullandığında başlangoç değerini vermen gerekirken
+            //out anahtar sözcüğü ile ilk değeri ataman gerekmez.
+
+        }
+
+        static void Topla(int sayi)
+        {
+            sayi = sayi + sayi;
+        }
+
+        static void Topla2(ref int sayi)
+        {
+            sayi = sayi + sayi;
+        }
+    }
+}
